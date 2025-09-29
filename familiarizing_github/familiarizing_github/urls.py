cca621f81904723a249b0e7dcd9a15d7f7ea4855
LAB23_USER ON WS07 <L23X15W07@LabHCI.net>
2025-09-29 17:23:11 +0800

    changes everything

--- familiarizing_github/familiarizing_github/urls.py
+++ familiarizing_github/familiarizing_github/urls.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("blood_bridge_app.urls")),  # ✅ connect app URLs
 ]
